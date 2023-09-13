a6a465f2ebe86197c839c53ce3cc85152b166c15
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-09-13 17:48:21 +0200

    [add] Bonus- Thumb

--- js/script.js
+++ js/script.js
@@ -8,22 +8,31 @@ const imagesArray = [
 const imagesContainer = document.querySelector('.images-lf');
 const nextButton = document.querySelector('.bottom-lf');
 const prevButton = document.querySelector('.up-lf');
+const thumbNail = document.querySelector('.thumb-nail-lf');
 
 let counterImg = 0;
 
 
 imagesContainer.innerHTML = '';
+thumbNail.innerHTML = '';
 
 for(let i = 0; i < imagesArray.length; i++){
   const img = imagesArray[i];
   imagesContainer.innerHTML += `
   <img src="${img}" class="item d-none" alt="0${i + 1}" >
   `;
+  thumbNail.innerHTML += `
+  <div class="img-card-lf">
+    <img src="${img}" alt="${i + 1}">
+  </div>
+  `;
 }
 
 const itemsCollection = document.getElementsByClassName('item');
+const thumbCollection = document.getElementsByClassName('img-card-lf');
 
 itemsCollection[counterImg].classList.remove('d-none');
+thumbCollection[counterImg].classList.add('active-lf');
 
 nextButton.addEventListener('click', nextImg);
 prevButton.addEventListener('click', prevImg);
@@ -36,7 +45,9 @@ function nextImg (){
   prevButton.classList.remove('d-none');
 
   itemsCollection[counterImg].classList.add('d-none');
+  thumbCollection[counterImg].classList.remove('active-lf');
   counterImg++;
+  thumbCollection[counterImg].classList.add('active-lf');
   itemsCollection[counterImg].classList.remove('d-none');
 
   if(counterImg === itemsCollection.length - 1){
@@ -48,8 +59,10 @@ function prevImg () {
   nextButton.classList.remove('d-none');
 
   itemsCollection[counterImg].classList.add('d-none');
+  thumbCollection[counterImg].classList.remove('active-lf');
   counterImg--;
   itemsCollection[counterImg].classList.remove('d-none');
+  thumbCollection[counterImg].classList.add('active-lf');
 
   if(counterImg === 0){
     prevButton.classList.add('d-none');
