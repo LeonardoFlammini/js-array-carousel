dcf9d8951a923959da3d9ed685eff5084dbd1216
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-09-13 17:23:13 +0200

    [add] consegna base

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const imagesArray = [
+  "img/01.webp",
+  "img/02.webp",
+  "img/03.webp",
+  "img/04.webp",
+  "img/05.webp"
+]
+const imagesContainer = document.querySelector('.images-lf');
+const nextButton = document.querySelector('.bottom-lf');
+const prevButton = document.querySelector('.up-lf');
+
+let counterImg = 0;
+
+
+imagesContainer.innerHTML = '';
+
+for(let i = 0; i < imagesArray.length; i++){
+  const img = imagesArray[i];
+  imagesContainer.innerHTML += `
+  <img src="${img}" class="item d-none" alt="0${i + 1}" >
+  `;
+}
+
+const itemsCollection = document.getElementsByClassName('item');
+
+itemsCollection[counterImg].classList.remove('d-none');
+
+nextButton.addEventListener('click', nextImg);
+prevButton.addEventListener('click', prevImg);
+
+prevButton.classList.add('d-none');
+
+
+function nextImg (){
+  // console.log('next');
+  prevButton.classList.remove('d-none');
+
+  itemsCollection[counterImg].classList.add('d-none');
+  counterImg++;
+  itemsCollection[counterImg].classList.remove('d-none');
+
+  if(counterImg === itemsCollection.length - 1){
+    nextButton.classList.add('d-none');
+  }
+}
+
+function prevImg () {
+  nextButton.classList.remove('d-none');
+
+  itemsCollection[counterImg].classList.add('d-none');
+  counterImg--;
+  itemsCollection[counterImg].classList.remove('d-none');
+
+  if(counterImg === 0){
+    prevButton.classList.add('d-none');
+  }
+}
+
+
+
